abf35f4085f017abd960e9754fbabfb147e32660
James Wu <ichangjwu@outlook.com>
2021-11-07 02:59:22 +0900

    completed basic user interface

---
+++ script.js
@@ -0,0 +1,35 @@
+let result = 0;
+
+function add(a, b) {
+  return a + b;
+};
+
+function subtract(a, b) {
+  return a - b;
+};
+
+function multiply(a, b) {
+  return a * b;
+};
+
+function divide(a, b) {
+  return a / b;
+};
+
+function operate(a, operator, b) {
+  switch (operator) {
+    case "+":
+      result = add(a, b);
+      break;
+    case "-":
+      result = subtract(a, b);
+      break;
+    case "*":
+      result = multiply(a, b);
+      break;
+    case "/":
+      result = divide(a, b);
+      break;
+  }
+  return result;
+}
