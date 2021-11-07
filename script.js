9dc10856ae63b34abbbee541bfb4170b5ea4076a
James Wu <ichangjwu@outlook.com>
2021-11-07 19:10:00 +0900

    worked on addition and subtraction

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let result = 0;
 
 function add(a, b) {
-  return a + b;
+  return Number(a) + Number(b);
 };
 
 function subtract(a, b) {
@@ -36,7 +36,10 @@ function operate(a, operator, b) {
 
 const digits = document.querySelectorAll("button.digits");
 const display = document.querySelector("div#display");
-const operators = document.querySelectorAll("button.operators")
+const operators = document.querySelectorAll("button.operators");
+const clear = document.querySelector("button#clear");
+const equal = document.querySelector("button#equal");
+let storedValue = ""
 
 digits.forEach(element => element.addEventListener('click', displayonedigit));
 
@@ -44,14 +47,26 @@ function displayonedigit(e) {
   display.textContent = e.target.textContent;
   digits.forEach(element => element.addEventListener('click', joindigits));
   digits.forEach(element => element.removeEventListener('click', displayonedigit));
-
 };
 
 function joindigits(e) {
   display.textContent += e.target.textContent;
 };
 
-operators.forEach(element => element.addEventListener('click', () => {
-  digits.forEach(element => element.removeEventListener('click', joindigits));
-  digits.forEach(element => element.addEventListener('click', displayonedigit));
+operators.forEach(operator => operator.addEventListener('click', (e) => {
+  display.textContent = operate(storedValue, e.target.textContent, display.textContent);
+  storedValue = display.textContent;
+  digits.forEach(digit => digit.removeEventListener('click', joindigits));
+  digits.forEach(digit => digit.addEventListener('click', displayonedigit));
 }));
+
+clear.addEventListener('click', () => {
+  display.textContent = 0;
+  digits.forEach(digit => digit.removeEventListener('click', joindigits));
+  digits.forEach(digit => digit.addEventListener('click', displayonedigit));
+});
+
+equal.addEventListener('click', () => {
+  digits.forEach(digit => digit.removeEventListener('click', joindigits));
+  digits.forEach(digit => digit.addEventListener('click', displayonedigit));
+});
